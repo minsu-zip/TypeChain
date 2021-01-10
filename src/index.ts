8c7e5a4d141aa478d42a7446fccad084f32098f0
minsu-zip <alstn4814@gmail.com>
2021-01-10 21:23:45 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import * as  CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (index: number,
+        hash: string,
+        previousHash: string,
+        timestamp: number,
+        data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(index: number,
         hash: string,
@@ -18,7 +26,20 @@ class Block {
     }
 }
 
+
+
 const genesisBlock: Block = new Block(0, "202020", "", "Hello", 123456);
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+    const previosBlock: Block = getLatestBlock();
+    const newIndex: number = previosBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
+
+}
 export { }
